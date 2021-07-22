6861771337a14e4764cc46ec3baf6e13f028fb1e
Krzysiek31415 <krzysztof.ciecko.kontakt@gmail.com>
2021-07-22 20:00:39 +0200

    version 1.0

--- Student.hpp
+++ Student.hpp
@@ -1,4 +1,7 @@
+#pragma once
 #include<string>
+#include<iostream>
+#include <iomanip>
 class Student
 {
     enum class Gender{male, female, other};
@@ -6,13 +9,14 @@ class Student
     std::string name_;
     std::string surname_;
     std::string address_;
-    unsigned int indexNumber_;
-    unsigned int PESEL_;
+    unsigned long indexNumber_;
+    unsigned long PESEL_;
     Gender gender_;
 
 public:
+    Student(){}
     Student(std::string name, std::string surname, std::string address,
-    unsigned int indexNumber, unsigned int PESEL, Gender gender)
+    unsigned long indexNumber, unsigned long PESEL, Gender gender)
     :   name_(name),
         surname_(surname),
         address_(address),
@@ -21,20 +25,29 @@ public:
         gender_(gender)
         {}
     Student(std::string name, std::string surname)
-    :   Student(name, surname, "", 0, 0, Gender::other)
+    :   Student(name, surname, "", 0UL, 0UL, Gender::other)
         {}
     void setName(std::string name){name_ = name;}
     void setSurname(std::string surname){surname_ = surname;}
     void setAddress(std::string address){address_ = address;}
-    void setIndexNumber(unsigned int indexNumber){indexNumber_= indexNumber;}
-    void setPESEL(unsigned int PESEL){PESEL_= PESEL;}
+    void setIndexNumber(unsigned long indexNumber){indexNumber_= indexNumber;}
+    void setPESEL(unsigned long PESEL){PESEL_= PESEL;}
     void setGender(Gender gender){gender_ = gender;}
 
     std::string getName() const {return name_;}
     std::string getSurname() const {return surname_;}
     std::string getAddress() const {return address_;}
-    unsigned int getIndexNumber() const {return indexNumber_;}
-    unsigned int getPESEL() const {return PESEL_;}
+    unsigned long getIndexNumber() const {return indexNumber_;}
+    unsigned long getPESEL() const {return PESEL_;}
     Gender getGender() const {return gender_;}
 
+    void showStudent()
+    {
+        std::cout<<std::left<<std::setw(13)<<getName()<<"| ";
+        std::cout<<std::left<<std::setw(13)<<getSurname()<<"| ";
+        std::cout<<std::left<<std::setw(13)<<getAddress()<<"| ";
+        std::cout<<std::left<<std::setw(13)<<getIndexNumber()<<"| ";
+        std::cout<<std::left<<std::setw(13)<<getPESEL()<<"| ";
+        std::cout<<std::left<<std::setw(13)<<(int)getGender()<<"| \n";
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include "Student.hpp"
+#include "UniversityDb.hpp"
 
 int main()
 {
-    std::cout<<"Hallo.";
+    Student Jan("Jan","Nowak");
+    Jan.setPESEL(92909008768);
+    Jan.setIndexNumber(182696UL);
+    //Jan.showStudent();
+    Student Ula("Urszula","Nowak");
+    Ula.setIndexNumber(192696UL);
+    //std::cout<<Jan.getName()<<"\n";
+    //std::cout<<Jan.getPESEL()<<"\n";
+    Student John;
+    Student Paul("Pawel", "Jasinski");
+    Student JanB("Jan","Brzechwa");
+    //std::cout<<John.getName()<<"\n";
+    UniversityDb PWr("Politechnika Wroclawska");
+    PWr.addStudent(JanB);
+    PWr.addStudent(Jan);
+    //PWr.addStudent(John);
+    Jan.showStudent();
+    PWr.addStudent(Paul);
+    PWr.addStudent(Ula);
+    //PWr.printUniversity();
+    PWr.printStudent(0);
+    //PWr.searchBySurname("Nowak");
+    //PWr.searchByPESEL(92909008768);
+    //auto it = PWr.searchByIndexNumber(182696UL);
+    //it->showStudent();
+    PWr.printUniversity();
+    //PWr.deleteByIndexNumber2(192696UL);
+    //PWr.sortBySurname  ();
+    //PWr.printUniversity();
+
     return 0;
 }
